ecd80225359ae62327866cc648efd0bd6616e3fa
Tim Asp <timothy.asp@gmail.com>
2012-11-13 19:51:36 -0800

    Renamed project to fbackup

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Facebook Photo Downloader',
+    'description': 'Personal Facebook Backup',
     'author': 'Timothy Asp',
-    'url': 'https://github.com/timothyasp/fb-photo-grab',
-    'download_url': 'https://github.com/timothyasp/fb-photo-grab',
+    'url': 'https://github.com/timothyasp/fbackup',
+    'download_url': 'https://github.com/timothyasp/fbackup',
     'author_email': 'timothy.asp@gmail.com',
     'version': '0.1',
     'install_requires': ['nose', 'facepy', 'urllib2'],
-    'packages': ['fphoto'],
-    'name': 'fphoto'
+    'packages': ['fbackup'],
+    'name': 'fbackup'
 }
 
 setup(**config)
