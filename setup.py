5e0923e64383e03774ced15e53569cb45860b8dc
Tim Asp <timothy.asp@gmail.com>
2012-11-13 20:34:53 -0800

    removed urllib from setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/timothyasp/fbackup',
     'author_email': 'timothy.asp@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose', 'facepy', 'urllib2'],
+    'install_requires': ['nose', 'facepy'],
     'packages': ['fbackup'],
     'name': 'fbackup'
 }
